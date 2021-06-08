1259af4dc50ef4c0116e8d850b84893563f81679
25011908vardhan <106120007@nitt.edu>
2021-06-08 14:08:23 +0530

    Normal Mode Completed

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 
         var index;
         var b;
-    
+        
     function my(){
+        var restart=document.getElementById("rest");
+        restart.addEventListener('click',()=>{
+            location.reload();
+        })
         var ans=[];
+        var scoring=document.getElementById('score');
         var arr= document.getElementsByClassName("work");
         console.log(arr);   
-        var colors=["#FF0000","#00FFFF","#00FF7F","#FFA500","#FFFFFF","#FFFF00"];
+        var colors=["#FDF200","#08F7FE","#00FF7F","#FFA500","#FFFFFF","#FF2281"];
         console.log("Hello there!");
         
         var element= document.getElementsByClassName("ques");
@@ -30,7 +35,8 @@
             arr[uniqRand[j]].style.backgroundColor=colors[Math.floor(Math.random() * 6)];
             
         }
-        arr[uniqRand[24]].style.backgroundColor="grey";
+        var score=0;
+        arr[uniqRand[24]].style.backgroundColor="black";
         index=uniqRand[24];
         b= arr[index];
         console.log(b);
@@ -40,47 +46,65 @@
         var down= document.getElementById("btn-d");
         up.addEventListener('click',()=>{
             b.style.backgroundColor=arr[index-5].style.backgroundColor;
-                    arr[index-5].style.backgroundColor="grey";
+                    arr[index-5].style.backgroundColor="black";
                     index=index-5;
                     b=arr[index];
                     if(check(arr,element)){
                         alert("Congratulations You have won the game!");
-                       location.reload();
+                        var reload=prompt("Restart immediately?\nYes->1 No->0")
+                        if(reload)
+                        {location.reload();}
                     }
+                    score++;
+                    scoring.innerHTML=score;
         })
        down.addEventListener('click',()=>{
         if((index+5)<25)
            { b.style.backgroundColor=arr[index+5].style.backgroundColor;
-                    arr[index+5].style.backgroundColor="grey";
+                    arr[index+5].style.backgroundColor="black";
                     index=index+5;
                     b=arr[index];
                     if(check(arr,element)){
                         alert("Congratulations You have won the game!");
-                        location.reload();
-                    }}
+                        
+                        var reload=prompt("Restart immediately?\nYes->1 No->0")
+                        if(reload)
+                        {location.reload();}
+                    }
+                    score++;
+                    scoring.innerHTML=score;
+                }
         })
         left.addEventListener('click',()=>{
             if((index-1)!=4 && (index-1)!=9 && (index-1)!=14 && (index-1)!=19 && (index-1)!=24)
             {b.style.backgroundColor=arr[index-1].style.backgroundColor;
-                    arr[index-1].style.backgroundColor="grey";
+                    arr[index-1].style.backgroundColor="black";
                     index=index-1;
                     b=arr[index];
                     if(check(arr,element)){
                         alert("Congratulations You have won the game!");
-                        location.reload();
-                    }}
+                        var reload=prompt("Restart immediately?\nYes->1 No->0")
+                        if(reload)
+                        {location.reload();}
+                    }
+                    score++;
+                    scoring.innerHTML=score;}
 
         })
         right.addEventListener('click',()=>{
             if((index+1)%5!=0)
             {b.style.backgroundColor=arr[index+1].style.backgroundColor;
-                    arr[index+1].style.backgroundColor="grey";
+                    arr[index+1].style.backgroundColor="black";
                     index=index+1;
                     b=arr[index];
                     if(check(arr,element)){
                         alert("Congratulations You have won the game!");
-                        location.reload();
-                    }}
+                        var reload=prompt("Restart immediately?\nYes->1 No->0")
+                        if(reload)
+                        {location.reload();}
+                    }
+                    score++;
+                    scoring.innerHTML=score;}
         })
                                                 
 }
